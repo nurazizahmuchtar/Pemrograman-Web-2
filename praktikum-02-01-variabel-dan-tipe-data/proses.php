ecbb8b83c9ed809ae812d13a136213dd2fea0188
nurazizahmuchtar <nurazizah.na837@gmail.com>
2022-10-31 20:01:24 +0800

    tugas 02-01

---
+++ praktikum-02-01-variabel-dan-tipe-data/proses.php
@@ -0,0 +1,13 @@
+<?php
+
+$nama = isset($_POST ['nama'])?$_POST['nama']:"";
+$nim = isset($_POST ['nim'])?$_POST['nim']:"";
+$studi = isset($_POST ['studi'])?$_POST['studi']:"";
+
+if ($nim == ""|| $nama == ""|| $studi ==""){
+    echo "<p><h3> Isi semua data, Klik <a href='input.html'> di sini <a/> untuk kembali</h3></p> ";
+} else {
+    echo " hai perkenalkan, nama saya ".$nama, "dengan nim :".$nim, "Saya mahasiswa dari program studi ".$studi;
+}
+
+?>
---
+++ praktikum-02-01-variabel-dan-tipe-data/variabel.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Ini adalah nilai awal variabel
+ */
+$nama = "Pasnur";
+$usia = 42;
+echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya berusia ".$usia." tahun<p>";
+
+/**
+ * Ini adalah variabel dengan nilai yang diubah
+ */
+
+$nama = "Azizah";
+$usia = 20;
+
+echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya berusia ".$usia." tahun<p>";
+
+ ?>
